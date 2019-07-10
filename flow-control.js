b525d2d6064ff30e583b2bf96d128c88e8841378
energetic-port-0799 <m.tarrats@yahoo.com>
2019-07-10 00:06:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,9 @@
 function basicTeenager(age) {
-if (13) {
-return "You are a teenager!"
-}
+
 }
 
 function teenager(age) {
-if (13) {
-return "You are a teenager!"
-}
-else if (12) {
-return "You are not a teenager"
-}
-else if (29) {
-return "You are not a teenager"
-}
+
 }
 
 function ageChecker(age) {
