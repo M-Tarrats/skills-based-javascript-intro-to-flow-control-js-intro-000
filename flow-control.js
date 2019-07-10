96a4b001db1238e4f2a7c32c89f37ad1a6f11108
energetic-port-0799 <m.tarrats@yahoo.com>
2019-07-10 00:44:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,10 +18,10 @@ function ageChecker(age) {
   if ((age) >= 13 && (age) <= 19) {
   return "You are a teenager!"
   }
-  else if (age <= 12) {
+  else if ((age) <= 12) {
     return "You are a kid"
   }
-  else if (age >= 29) {
+  else if ((age) >= 29) {
     return "You are a grownup"
   }
 }
