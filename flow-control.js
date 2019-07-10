738dd0d5b44662fda5e6916c94a58d07c46a372f
energetic-port-0799 <m.tarrats@yahoo.com>
2019-07-10 00:33:21 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -9,6 +9,9 @@ function teenager(age) {
   if ((age) >= 13 && (age) <= 19) {
   return "You are a teenager!"
   }
+  else if (12) {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
